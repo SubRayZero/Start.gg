efdfd4c158b2c7baeadda570592d1b91ac05f179
SubRayZero <jasmine.draperi.simplon@gmail.com>
2024-04-24 11:24:04 +0200

    css header ok

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,6 +18,10 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
+            ->add('pseudo')
+            ->add('firstname')
+            ->add('name')
+
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
