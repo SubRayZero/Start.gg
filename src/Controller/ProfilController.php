39032a94d02e3164d2264467e229b30caa835af2
SubRayZero <jasmine.draperi.simplon@gmail.com>
2024-05-03 15:43:22 +0200

    modify user ok

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -20,6 +22,26 @@ class ProfilController extends AbstractController
         return $this->render('profil/index.html.twig', [
             'controller_name' => 'ProfilController',
             'users' => $users
+        ]);
+    }
+
+    #[Route('/profil/modify', name: 'app_modify_profil')]
+    public function profilModify(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(RegistrationFormType::class, $user);;
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_profil');
+        }
+
+        return $this->render('profil/modify.html.twig', [
+            'controller_name' => 'ProfilController',
+            'form' => $form,
         ]);
     }
 }
